bcb8a107a5bc58d60d35e146f5fa507cbb298f26
Amaru333 <amrutesharun0599@gmail.com>
2024-07-08 22:27:05 +0530

    Video player with questionnaire completed

---
+++ src/constants/dummyData.ts
@@ -0,0 +1,26 @@
+export const DUMMY_QUESTIONS = [
+  {
+    timeStamp: 13,
+    question: "What is the capital of France?",
+    options: ["Paris", "London", "Berlin", "Madrid"],
+    correctAnswer: "Paris",
+  },
+  {
+    timeStamp: 25,
+    question: "What is the capital of Germany?",
+    options: ["Paris", "London", "Berlin", "Madrid"],
+    correctAnswer: "Berlin",
+  },
+  {
+    timeStamp: 37,
+    question: "What is the capital of Spain?",
+    options: ["Paris", "London", "Berlin", "Madrid"],
+    correctAnswer: "Madrid",
+  },
+  {
+    timeStamp: 49,
+    question: "What is the capital of England?",
+    options: ["Paris", "London", "Berlin", "Madrid"],
+    correctAnswer: "London",
+  },
+];
