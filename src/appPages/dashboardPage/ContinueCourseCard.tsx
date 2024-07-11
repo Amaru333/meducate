7e343fcec4cb1f6b353e30fe711abd2c9c911b16
Amaru333 <amrutesharun0599@gmail.com>
2024-07-11 23:19:45 +0530

    watch next course

--- src/appPages/dashboardPage/ContinueCourseCard.tsx
+++ src/appPages/dashboardPage/ContinueCourseCard.tsx
@@ -10,11 +10,12 @@ import Link from "next/link";
 import React, { useEffect } from "react";
 
 function ContinueCourseCard() {
-  const [courseDetails, setCourseDetails] = React.useState();
+  const [courseDetails, setCourseDetails] = React.useState<any>();
   useEffect(() => {
     httpRequest
       .get(RECENTLY_WATCHED_COURSE_ENDPOINT)
       .then((res) => {
+        setCourseDetails(res.data);
         console.log(res);
       })
       .catch((err) => console.log(err));
@@ -25,19 +26,19 @@ function ContinueCourseCard() {
         <Image alt="thumbnail" src="/images/stock-thumbnail-1.png" width={300} height={0} sizes="100vw" className="object-contain rounded-2xl" />
         <div className="pl-4 flex flex-col">
           <div className="flex-1">
-            <p className="text-2xl font-medium">How to perform a surgery</p>
+            <p className="text-2xl font-medium">{courseDetails?.title}</p>
             <p className="text-sm">
-              Lecture 3: {DUMMY_COURSE_ITEMS[2].title} <span className="text-xs font-light text-gray-500">({formatSeconds(DUMMY_COURSE_ITEMS[2].duration)} mins)</span>
+              Lecture {courseDetails?.lectureToWatch?.index}: {courseDetails?.lectureToWatch?.lecture?.title} <span className="text-xs font-light text-gray-500">({formatSeconds(DUMMY_COURSE_ITEMS[2].duration)} mins)</span>
             </p>
-            <p className="line-clamp-2 text-sm my-2">{DUMMY_COURSE_ITEMS[2].description}</p>
-            <Progress value={72} className="h-2 mt-4" />
-            <p className="italic text-xs font-light text-gray-500 mt-1">72% completed</p>
+            <p className="line-clamp-2 text-sm my-2">{courseDetails?.description}</p>
+            <Progress value={parseFloat(courseDetails?.progress)} className="h-2 mt-4" />
+            <p className="italic text-xs font-light text-gray-500 mt-1">{courseDetails?.progress}% completed</p>
           </div>
           <div className="flex gap-x-4 mt-4 text-xs">
-            <Link href="/dashboard/courses/how-to-do-surgery/" className="px-4 py-2 text-primary rounded-xl bg-opacity-100 hover:bg-opacity-90 transition-all active:scale-95">
+            <Link href={"/dashboard/courses/" + courseDetails?.slug} className="px-4 py-2 text-primary rounded-xl bg-opacity-100 hover:bg-opacity-90 transition-all active:scale-95">
               Start Over
             </Link>
-            <Link href="/dashboard/courses/how-to-do-surgery/3" className="bg-primary px-4 py-2 text-white rounded-xl bg-opacity-100 hover:bg-opacity-90 transition-all active:scale-95">
+            <Link href={"/dashboard/courses/" + courseDetails?.slug + "/" + courseDetails?.lectureToWatch?.lecture?._id} className="bg-primary px-4 py-2 text-white rounded-xl bg-opacity-100 hover:bg-opacity-90 transition-all active:scale-95">
               Continue
             </Link>
           </div>
